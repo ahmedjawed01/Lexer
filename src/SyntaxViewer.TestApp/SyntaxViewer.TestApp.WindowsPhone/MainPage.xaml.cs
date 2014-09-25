cfaa1b264c951420b5e52c4355115451f2d260a1
Adnan Umer <aztnan@outlook.com>
2014-09-25 11:30:28 +0500

    init

---
+++ src/SyntaxViewer.TestApp/SyntaxViewer.TestApp.WindowsPhone/MainPage.xaml.cs
@@ -0,0 +1,135 @@
+﻿// Copyright (c) Adnan Umer. All rights reserved. Follow me @aztnan
+// Email: aztnan@outlook.com
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+// software and associated documentation files (the "Software"), to deal in the Software
+// without restriction, including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+// to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+using SyntaxViewer.TestApp.Common;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Display;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Basic Page item template is documented at http://go.microsoft.com/fwlink/?LinkID=390556
+
+namespace SyntaxViewer.TestApp
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        private NavigationHelper navigationHelper;
+        private ObservableDictionary defaultViewModel = new ObservableDictionary();
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.navigationHelper = new NavigationHelper(this);
+            this.navigationHelper.LoadState += this.NavigationHelper_LoadState;
+            this.navigationHelper.SaveState += this.NavigationHelper_SaveState;
+        }
+
+        /// <summary>
+        /// Gets the <see cref="NavigationHelper"/> associated with this <see cref="Page"/>.
+        /// </summary>
+        public NavigationHelper NavigationHelper
+        {
+            get { return this.navigationHelper; }
+        }
+
+        /// <summary>
+        /// Gets the view model for this <see cref="Page"/>.
+        /// This can be changed to a strongly typed view model.
+        /// </summary>
+        public ObservableDictionary DefaultViewModel
+        {
+            get { return this.defaultViewModel; }
+        }
+
+        /// <summary>
+        /// Populates the page with content passed during navigation.  Any saved state is also
+        /// provided when recreating a page from a prior session.
+        /// </summary>
+        /// <param name="sender">
+        /// The source of the event; typically <see cref="NavigationHelper"/>
+        /// </param>
+        /// <param name="e">Event data that provides both the navigation parameter passed to
+        /// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested and
+        /// a dictionary of state preserved by this page during an earlier
+        /// session.  The state will be null the first time a page is visited.</param>
+        private void NavigationHelper_LoadState(object sender, LoadStateEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Preserves state associated with this page in case the application is suspended or the
+        /// page is discarded from the navigation cache.  Values must conform to the serialization
+        /// requirements of <see cref="SuspensionManager.SessionState"/>.
+        /// </summary>
+        /// <param name="sender">The source of the event; typically <see cref="NavigationHelper"/></param>
+        /// <param name="e">Event data that provides an empty dictionary to be populated with
+        /// serializable state.</param>
+        private void NavigationHelper_SaveState(object sender, SaveStateEventArgs e)
+        {
+        }
+
+        #region NavigationHelper registration
+
+        /// <summary>
+        /// The methods provided in this section are simply used to allow
+        /// NavigationHelper to respond to the page's navigation methods.
+        /// <para>
+        /// Page specific logic should be placed in event handlers for the  
+        /// <see cref="NavigationHelper.LoadState"/>
+        /// and <see cref="NavigationHelper.SaveState"/>.
+        /// The navigation parameter is available in the LoadState method 
+        /// in addition to page state preserved during an earlier session.
+        /// </para>
+        /// </summary>
+        /// <param name="e">Provides data for navigation methods and event
+        /// handlers that cannot cancel the navigation request.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            this.navigationHelper.OnNavigatedTo(e);
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            this.navigationHelper.OnNavigatedFrom(e);
+        }
+
+        #endregion
+
+        private void OnTextChanged(object sender, TextChangedEventArgs e)
+        {
+            SyntaxViewer.Text = ((TextBox)sender).Text;
+        }
+    }
+}
---
+++ src/SyntaxViewer/SyntaxViewer.cs
@@ -0,0 +1,147 @@
+﻿// Copyright (c) Adnan Umer. All rights reserved. Follow me @aztnan
+// Email: aztnan@outlook.com
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this
+// software and associated documentation files (the "Software"), to deal in the Software
+// without restriction, including without limitation the rights to use, copy, modify, merge,
+// publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons
+// to whom the Software is furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all copies or
+// substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+// INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+// PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE
+// FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+// OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+// DEALINGS IN THE SOFTWARE.
+
+
+using Lexer;
+using System.Collections.Generic;
+using System.Text;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Documents;
+using Windows.UI.Xaml.Media;
+
+namespace SyntaxViewer
+{
+    public class SyntaxViewer : Control
+    {
+        public SyntaxViewer()
+        {
+            DefaultStyleKey = typeof(SyntaxViewer);
+
+            TextView = new RichTextBlock { /* FontSize = 13, FontFamily = new FontFamily("Consolas") */ };
+            LineNumberBlock = new TextBlock { /*FontSize = 13, FontFamily = new FontFamily("Consolas"),  */ Foreground = new SolidColorBrush(Color.FromArgb(255, 43, 145, 175)) };
+
+        }
+
+        #region TextView
+
+        public static DependencyProperty TextViewProperty =
+            DependencyProperty.Register("TextView", typeof(RichTextBlock), typeof(SyntaxViewer),
+                                        new PropertyMetadata(null));
+
+        public RichTextBlock TextView
+        {
+            get { return (RichTextBlock)GetValue(TextViewProperty); }
+            set { SetValue(TextViewProperty, value); }
+        }
+
+        #endregion
+
+        #region Line No Margin
+
+        public static DependencyProperty LineNumberBlockProperty =
+            DependencyProperty.Register("LineNumberBlock", typeof(TextBlock), typeof(SyntaxViewer),
+                                        new PropertyMetadata(null));
+
+        public TextBlock LineNumberBlock
+        {
+            get { return (TextBlock)GetValue(LineNumberBlockProperty); }
+            set { SetValue(LineNumberBlockProperty, value); }
+        }
+
+        #endregion
+
+        #region Text
+
+        public static DependencyProperty TextProperty = DependencyProperty.Register("Text", typeof(string), typeof(SyntaxViewer), new PropertyMetadata("", OnTextProperyChanged));
+
+        private static void OnTextProperyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((SyntaxViewer)d).OnTextChanged((string)e.NewValue);
+        }
+
+        public string Text
+        {
+            get { return GetValue(TextProperty).ToString(); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        Tokenizer lexer = new Tokenizer(new Lexer.Grammers.PythonGrammer());
+
+        Dictionary<TokenType, Brush> ColorDict = new Dictionary<TokenType, Brush>()
+        {
+            { TokenType.Comment, new SolidColorBrush(Color.FromArgb(255, 221, 0, 0)) },
+            { TokenType.String, new SolidColorBrush(Color.FromArgb(255, 0, 171, 0)) },
+            { TokenType.Builtins, new SolidColorBrush(Color.FromArgb(255, 144, 0, 144)) },
+            { TokenType.Keyword, new SolidColorBrush(Color.FromArgb(255, 255, 119, 0)) },
+        };
+
+        int lines;
+        protected void OnTextChanged(string value)
+        {
+            if (TextView == null || value == null) return;
+
+            lines = value.Split('\n').Length;
+            StringBuilder builder = new StringBuilder();
+            for (int i = 1; i <= lines; i++)
+            {
+                builder.Append(i);
+                builder.Append("\n");
+            }
+
+            LineNumberBlock.Text = builder.ToString();
+            builder.Clear();
+
+            TextView.Blocks.Clear();
+            var p = new Paragraph();
+            TextView.Blocks.Add(p);
+
+            Brush brush;
+
+            foreach (var token in lexer.Tokenize(value))
+            {
+                if (ColorDict.TryGetValue(token.Type, out brush))
+                {
+                    if (builder.Length > 0)
+                    {
+                        p.Inlines.Add(new Run { Text = builder.ToString() });
+                        builder.Clear();
+                    }
+
+                    p.Inlines.Add(new Run
+                    {
+                        Text = value.Substring(token.StartIndex, token.Length),
+                        Foreground = brush
+                    });
+                }
+                else
+                    builder.Append(value.Substring(token.StartIndex, token.Length));
+            }
+
+            if (builder.Length > 0)
+            {
+                p.Inlines.Add(new Run { Text = builder.ToString() });
+                builder.Clear();
+            }
+        }
+
+        #endregion
+    }
+}
